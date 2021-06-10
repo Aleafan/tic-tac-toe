c25d05e941acb573f10a26cd428334a0aa8ca676
Aleafan <ae.afanasov@yandex.com>
2021-06-10 21:26:43 +0300

    Add game.checkResult function

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,18 @@
 const gameBoard = (() => {
     const board = ['', '', '', '', '', '', '', '', ''];
-
 	const getBoard = () => board;
-
 	const renderBoard = () => {
 		const html = board.map((square, i) => `<div id='${i}'>${square}</div>`)
 				.join('');
 		container.innerHTML = html;
 	}
-
 	const updateBoard = (id, mark) => board.splice(id, 1, mark);
-
 	const validateSquare = (square) => {
 		const id = square.id;
 		if (id === 'gameboard') return false;
 		if (board[id]) return false;
 		return true;
 	}
-
     return {
 		getBoard,
 		renderBoard,
@@ -28,7 +23,6 @@ const gameBoard = (() => {
 
 const game = (() => {
 	let player1Turn = true;
-
 	const makeTurn = (e) => {
 		const square = e.target;
 		if (!gameBoard.validateSquare(square)) return;
@@ -36,8 +30,35 @@ const game = (() => {
 		square.textContent = player.mark;
 		gameBoard.updateBoard(square.id, player.mark);
 		player1Turn = !player1Turn;
+		checkResult();
 	}
-
+	const checkResult = () => {
+		const board = gameBoard.getBoard();
+		// Check for win
+		let mark = '';
+		if (board[0] && ((board[0] === board[1] && board[0] === board[2]) 
+					|| (board[0] === board[3] && board[0] === board[6])
+					|| (board[0] === board[4] && board[0] === board[8]))) {
+			mark = board[0];	
+		}
+		else if (board[4] && ((board[4] === board[3] && board[4] === board[5]) 
+					|| (board[4] === board[1] && board[4] === board[7])
+					|| (board[4] === board[2] && board[4] === board[6]))) {
+			mark = board[4];
+		}
+		else if (board[8] && ((board[8] === board[6] && board[8] === board[7]) 
+					|| (board[8] === board[2] && board[8] === board[5]))) {
+			mark = board[8];		
+		}
+		const message = mark === 'X' ? 'Player 1 wins!' 
+				: mark === 'O' ? 'Player 2 wins'
+				: null;
+		if (message) return console.log(message);
+		// Check for tie
+		if (!board.includes('')) {
+			return console.log("It's a tie!");
+		}
+	};
 	return {
 		makeTurn
 	}
